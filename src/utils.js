47f3c3b4eb69628ec427c41b2be003fd05a9c96d
Austin <me@austinwilliams.dev>
2022-01-19 00:13:56 -0800

    cleaned utils script

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,5 @@
-import { animatorConfig } from "./_config";
-
-// 1, 5
-// export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-export const getRandomNumber = max => Math.floor(Math.random() * max) + 1;
-
-// probability is the chance for a true value - 0.5 equals 50% chance to be true
+// probability is the chance for a true value — 0.5 equals 50% chance to be true
 export const getRandomBool = (probability = 0.5) => Math.random() < probability;
 
-// export const getRandomSize = (maxSize = animatorConfig.codeBlockMaxSize) => {
-//   if (maxSize > animatorConfig.codeBlockMaxSize)
-//     maxSize = animatorConfig.codeBlockMaxSize;
-//   return getRandomNumber(animatorConfig.codeBlockMinSize, maxSize);
-// }
+// max is maximum random value — e.g. max = 5 ... result range 1 through 5
+export const getRandomNumber = max => Math.floor(Math.random() * max) + 1;
