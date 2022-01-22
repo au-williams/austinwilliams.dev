61c893b25f5f96052a2f612ac1459f1c2cc777fd
Austin <me@austinwilliams.dev>
2022-01-21 20:49:02 -0800

    renamed files, added placeholder avatar

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,15 @@
 import "./Content.css";
 import React, { useEffect, useState } from "react";
+import AvatarPlaceholder from "../assets/avatar placeholder.svg"
 import Banner from "../assets/banner.png"
-import GitHubIcon from "../assets/github.svg"
-import ScrollIcon from "../assets/scroll.svg"
+import GitHubLogo from "../assets/github logo.svg"
 import MailboxEmoji from "../assets/mailbox emoji.png";
+import ScrollIcon from "../assets/scroll icon.svg"
 import WavingEmoji from "../assets/waving emoji.png";
 
 export default function Description(props) {
   const { isRef } = props;
-  const [avatar, setAvatar] = useState();
+  const [avatar, setAvatar] = useState(AvatarPlaceholder);
 
   useEffect(() => {
     fetch('https://api.github.com/users/au-williams')
@@ -20,12 +21,12 @@ export default function Description(props) {
   return (
     <div id='content-wrapper' ref={isRef}>
       <div className='flex-wrapper'>
-        {avatar && <img src={avatar} alt="avatar" draggable='false' />}
+        <img src={avatar} alt="avatar" draggable='false' />
         <div>Hey <img src={WavingEmoji} alt="waving emoji"/> — My name is <a href='https://www.linkedin.com/in/auwilliams'>Austin</a>. I started my career by developing government programs and collaborating with major tech companies, who taught me their art of delivering great software from start to finish.</div>
       </div>
       <div className='flex-wrapper'>
         <img src={Banner} alt="banner" draggable='false' />
-        <div>I love working with computers and I'm always open to new opportunities. Feel free to send an email for side-work or employment inquiries. You can reach me at <a href="mailto:me@austinwilliams.dev">me@austinwilliams.dev</a>. <img src={MailboxEmoji} alt="mailbox emoji"/></div>
+        <div>I love working with computers and I'm always open to new opportunities. Feel free to send an email for side-work or employment inquiries — you can reach me at <a href="mailto:me@austinwilliams.dev">me@austinwilliams.dev</a>. <img src={MailboxEmoji} alt="mailbox emoji"/></div>
       </div>
       <div id='footer'>
         <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
@@ -33,7 +34,7 @@ export default function Description(props) {
           Back to top
         </button>
         <a href='https://github.com/au-williams/au-williams.github.io' target="_blank" rel="noopener noreferrer">
-          <img src={GitHubIcon} alt="github logo"/>
+          <img src={GitHubLogo} alt="github logo"/>
           GitHub
         </a>
       </div>
