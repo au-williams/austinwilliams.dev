7fc8859ee2c3ff12561d0e7d785bc21e96898baa
Austin Williams <me@austinwilliams.dev>
2025-04-21 00:43:46 -0700

    updated components to tsx files, resolved eslintrc rules

--- src/components/code-block/code-block.tsx
+++ src/components/code-block/code-block.tsx
@@ -4,7 +4,27 @@ import React, { useEffect, useState } from 'react';
 import styles from './code-block.module.scss';
 import variables from '../code-window/code-window.module.scss';
 
-const CodeBlock = ({ blockType, currentSize, isActiveBlock, isColoredBlock }) => {
+/**
+ * Description placeholder
+ *
+ * @param {object} param0
+ * @param {*} param0.blockType
+ * @param {*} param0.currentSize
+ * @param {*} param0.isActiveBlock
+ * @param {*} param0.isColoredBlock
+ * @returns
+ */
+const CodeBlock = ({
+  blockType,
+  currentSize,
+  isActiveBlock,
+  isColoredBlock,
+}: {
+  blockType: any;
+  currentSize: any;
+  isActiveBlock: any;
+  isColoredBlock: any;
+}) => {
   const [isPushed, setIsPushed] = useState(false);
 
   // animate code block every time it increments its size
@@ -19,15 +39,20 @@ const CodeBlock = ({ blockType, currentSize, isActiveBlock, isColoredBlock }) =>
     blockType,
     { [styles.active]: isActiveBlock },
     { [styles.color]: isColoredBlock },
-    { [styles.pushed]: isPushed }
+    { [styles.pushed]: isPushed },
   );
 
   // calculate the width to perfectly align blocks across separate lines
   const calculatedWidth = `${variables.codeBlockSize} * ${currentSize}`;
   const calculatedSpace = `${variables.codeLineSpace} * ${currentSize * 2 - 2}`;
-  const style = currentSize > 1 ? { width: `calc(${calculatedWidth} + ${calculatedSpace})` } : null;
+  const style =
+    currentSize > 1
+      ? { width: `calc(${calculatedWidth} + ${calculatedSpace})` }
+      : undefined;
 
-  return <div className={classes} style={style} onAnimationEnd={onAnimationEnd} />;
+  return (
+    <div className={classes} style={style} onAnimationEnd={onAnimationEnd} />
+  );
 };
 
 CodeBlock.propTypes = {
