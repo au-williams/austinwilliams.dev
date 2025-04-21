44541d95f4f6e1887b3dde3f34b039463891b1fb
Austin Williams <me@austinwilliams.dev>
2025-04-21 01:46:42 -0700

    updated code comments in code block component

--- src/components/code-block/code-block.tsx
+++ src/components/code-block/code-block.tsx
@@ -5,14 +5,12 @@ import styles from './code-block.module.scss';
 import variables from '../code-window/code-window.module.scss';
 
 /**
- * Description placeholder
- *
- * @param {object} param0
- * @param {*} param0.blockType
- * @param {*} param0.currentSize
- * @param {*} param0.isActiveBlock
- * @param {*} param0.isColoredBlock
- * @returns
+ * The CodeBlock component. A single CodeLine component can have none to many nested CodeBlock components.
+ * @param {string} param.blockType The code block type (name value defined in code-block.module.scss)
+ * @param {number} param.currentSize The current code block size
+ * @param {boolean} param.isActiveBlock If this is the current code block being displayed
+ * @param {boolean} param.isColoredBlock If this code block is displayed in color
+ * @returns {React.JSX.Element}
  */
 const CodeBlock = ({
   blockType,
@@ -20,21 +18,22 @@ const CodeBlock = ({
   isActiveBlock,
   isColoredBlock,
 }: {
-  blockType: any;
-  currentSize: any;
-  isActiveBlock: any;
-  isColoredBlock: any;
-}) => {
+  blockType: string;
+  currentSize: number;
+  isActiveBlock: boolean;
+  isColoredBlock: boolean;
+}): React.JSX.Element => {
   const [isPushed, setIsPushed] = useState(false);
 
-  // animate code block every time it increments its size
+  // Animate the code block component each time its current size value changes.
   useEffect(() => {
     setIsPushed(true);
   }, [currentSize]);
 
-  // unbind on animation end so it can be replayed
+  // Unbind when the animation ends so the animation can be replayed as needed.
   const onAnimationEnd = () => setIsPushed(false);
 
+  // Create the CSS classes based on the component state.
   const classes = classNames(
     blockType,
     { [styles.active]: isActiveBlock },
@@ -45,14 +44,11 @@ const CodeBlock = ({
   // calculate the width to perfectly align blocks across separate lines
   const calculatedWidth = `${variables.codeBlockSize} * ${currentSize}`;
   const calculatedSpace = `${variables.codeLineSpace} * ${currentSize * 2 - 2}`;
-  const style =
-    currentSize > 1
-      ? { width: `calc(${calculatedWidth} + ${calculatedSpace})` }
-      : undefined;
 
-  return (
-    <div className={classes} style={style} onAnimationEnd={onAnimationEnd} />
-  );
+  let style: React.CSSProperties | undefined;
+  if (currentSize > 1) style = { width: `calc(${calculatedWidth} + ${calculatedSpace})` };
+
+  return <div className={classes} style={style} onAnimationEnd={onAnimationEnd} />;
 };
 
 CodeBlock.propTypes = {
