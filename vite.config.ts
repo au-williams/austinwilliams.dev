0a09e190856d6e225e6bc0151851d601cad06dd0
Austin Williams <me@austinwilliams.dev>
2025-05-01 22:15:18 -0700

    update vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => {
   setEnv(mode);
   return {
-		base: "/au-williams.github.io/",
+		base: "/",
     plugins: [
       react(),
       tsconfigPaths(),
